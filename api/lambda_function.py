2d24538b190938bf7bb64516e06b4f89674b1597
Kyle <kyle.scott.watson@gmail.com>
2017-09-04 18:48:57 -0400

    Api authenticates given code and will reject duplicate authentication attempts. Updated cft to allow db puts

--- api/lambda_function.py
+++ api/lambda_function.py
@@ -1,6 +1,7 @@
 import os
 import json
 import boto3
+import time
 
 table = os.environ['LegacyOneDatabase']
 dynamodb = boto3.client('dynamodb')
@@ -22,10 +23,18 @@ def handler(event, context):
         'headers': default_headers
       }
   else:
+    print('TOP')
     id = event['pathParameters']['id']
     response = dynamodb.get_item(TableName = table, Key = {'Id': {'N': id}})
     
-    return respond(None, 204 if 'Item' in response else 404)
+    if 'Item' not in response:
+      print('Item not in response')
+      return respond(None, 404)
+    elif 'authenticatedAt' in response['Item']:
+      return respond(None, 409)
+    else:
+      dynamodb.put_item(TableName = table, Item = {'Id': {'N': id}, 'authenticatedAt': {'S': str(time.time())}})
+      respond(None, 204)
 
 def respond(err, code=200):
     return {
